e502afb13810d595f2f699936ac0f55b195cd96d
Carlos Silva <carlos.uva.silva@gmail.com>
2022-05-04 14:01:50 +0100

    patch tags into container

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ async function run() {
     const imageURI = core.getInput('image', { required: true });
 
     const environmentVariables = core.getInput('environment-variables', { required: false });
+    const taskVariables = core.getInput('task-tags', { required: false });
 
     // Parse the task definition
     const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
@@ -66,6 +67,43 @@ async function run() {
         } else {
           // Else, create
           containerDef.environment.push(variable);
+        }
+      })
+    }
+
+    if (taskVariables) {
+
+      // If environment array is missing, create it
+      if (!Array.isArray(taskDefContents.tags)) {
+        taskDefContents.tags = [];
+      }
+
+      // Get pairs by splitting on newlines
+      taskVariables.split('\n').forEach(function (line) {
+        // Trim whitespace
+        const trimmedLine = line.trim();
+        // Skip if empty
+        if (trimmedLine.length === 0) { return; }
+        // Split on =
+        const separatorIdx = trimmedLine.indexOf("=");
+        // If there's nowhere to split
+        if (separatorIdx === -1) {
+            throw new Error(`Cannot parse the task variable '${trimmedLine}'. Task variable pairs must be of the form NAME=value.`);
+        }
+        // Build object
+        const variable = {
+          name: trimmedLine.substring(0, separatorIdx),
+          value: trimmedLine.substring(separatorIdx + 1),
+        };
+
+        // Search container definition environment for one matching name
+        const variableDef = taskDefContents.tags.find((e) => e.name == variable.name);
+        if (variableDef) {
+          // If found, update
+          variableDef.value = variable.value;
+        } else {
+          // Else, create
+          taskDefContents.tags.push(variable);
         }
       })
     }
